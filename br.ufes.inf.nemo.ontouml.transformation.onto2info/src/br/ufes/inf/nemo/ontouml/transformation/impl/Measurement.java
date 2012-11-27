599cde0955efeee58e1c2b411ce6cfd134a50e7d
rc.carraretto@gmail.com <rc.carraretto@gmail.com@0d781cd5-9cd4-e29e-cd9a-c3603b17ce84>
2012-11-27 21:21:17 +0000

    Onto2Info: measurement
    git-svn-id: http://rcarraretto.googlecode.com/svn/trunk@310 0d781cd5-9cd4-e29e-cd9a-c3603b17ce84

--- br.ufes.inf.nemo.ontouml.transformation.onto2info/src/br/ufes/inf/nemo/ontouml/transformation/impl/Measurement.java
+++ br.ufes.inf.nemo.ontouml.transformation.onto2info/src/br/ufes/inf/nemo/ontouml/transformation/impl/Measurement.java
@@ -15,7 +15,6 @@ public class Measurement
 		this.main = t;
 	}
 	
-	// TODO: history and time tracking
 	public void dealMeasurement ()
 	{
 		for (Entry<RefOntoUML.Class, MeasurementDecision> entry : main.dh.measurementMap.entrySet())
@@ -50,6 +49,10 @@ public class Measurement
 		}
 	}
 	
+	
+	
+	/* ------------------------------------ Measurement ------------------------------------ */
+	
 	private void qualityInScope (RefOntoUML.Quality q1, RefOntoUML.Class c1, MeasurementDecision decision, UMLAttributeSlot slot)
 	{
 		if (slot.measurementAttribute == null)
@@ -59,7 +62,7 @@ public class Measurement
 			if (main.dh.getHTPastDecision(q1))
 			{
 				// History Tracking = true
-				dealHistoryTracking(q1, c1, decision, slot);
+				addHistoryTracking(q1, c1, decision, slot);
 			}
 			else
 			{
@@ -72,92 +75,155 @@ public class Measurement
 		{
 			// Measurement attribute exists
 			
-			// Check if the attribute has changed
-			org.eclipse.uml2.uml.Type type = main.umlAbstraction.getMeasurementType (decision);
-			org.eclipse.uml2.uml.Type pastType = slot.measurementAttribute.getType();
-								
-			// If the past type was Custom
-			if (main.umlAbstraction.isCustomType(pastType))
-			{
-				// Simply delete the past CustomType (even if CustomType is chosen again FIXME)
-				main.umlAbstraction.removePackageableElement(slot.measurementAttribute.getType());
-			}
-				
-			if (pastType != type)
-			{
-				// A change of type happened	
-				// Update the type
-				slot.measurementAttribute.setType(type);
-				
-				// TODO: display in the ui that a change happened
-			}
+			// FIXME: history tracking was done? from ht=false to ht=true / from ht=true to ht=false
+			
+			
+			// Check for changes in names or data types
+			changesInAttribute(decision, slot);
+		}
+	}
+	
+	private void changesInAttribute(MeasurementDecision decision, UMLAttributeSlot slot)
+	{
+		// Check if the attribute has changed
+		org.eclipse.uml2.uml.Type type = main.umlAbstraction.getMeasurementType (decision);
+		org.eclipse.uml2.uml.Type pastType = slot.measurementAttribute.getType();
+							
+		// If the past type was Custom
+		if (main.umlAbstraction.isCustomType(pastType))
+		{
+			// Simply delete the past CustomType (even if CustomType is chosen again FIXME)
+			main.umlAbstraction.removePackageableElement(slot.measurementAttribute.getType());
+		}
+			
+		if (pastType != type)
+		{
+			// A change of type happened	
+			// Update the type
+			slot.measurementAttribute.setType(type);
+			
+			// TODO: display in the ui that a change happened
 		}
 	}
 	
 	private void addAttributeToCharacterizedType (RefOntoUML.Quality q1, RefOntoUML.Class c1, MeasurementDecision decision, UMLAttributeSlot slot)
 	{
 		slot.measurementAttribute = main.umlAbstraction.addMeasurementAttribute(q1, (org.eclipse.uml2.uml.Class) Onto2InfoMap.getElement(c1), decision);
-		main.ui.writeLog("Created UML.Property for " + c1.getName() + ": " + slot.measurementAttribute.getName());
+		
+		main.ui.writeLog("Created attribute for " + c1.getName() + ": " + slot.measurementAttribute.getName());
 		Log.addition();
-	}
-	
-	// TODO
-	private void dealHistoryTracking(RefOntoUML.Quality q1, RefOntoUML.Class c1, MeasurementDecision decision, UMLAttributeSlot slot)
-	{
-		if (slot.measureType == null)
-		{
-			// create "Measure Type"
-			slot.measureType = main.umlAbstraction.createMeasureType(q1.getName() + " Measure");
-			// add the "Measurement Attribute" to the "Measure type"
-			slot.measurementAttribute = main.umlAbstraction.addMeasurementAttribute(q1, slot.measureType, decision);
-			// create "Measure Association" (between the "Characterized Type" and the "Measure Type")
-			main.umlAbstraction.createMeasureAssociation((org.eclipse.uml2.uml.Class)Onto2InfoMap.getElement(c1), slot.measureType);
-			
-			if (main.dh.getStartTimeDecision(q1))
-			{
-				dealTimeTracking(slot);
-			}
-		}
-		else
-		{
-			// "Measure Type" already exists
-			// TODO
-		}
-	}
-	
-	private void dealTimeTracking(UMLAttributeSlot slot)
-	{
-		if (slot.startAttribute == null)
-		{
-			// create "Time Attribute"
-			main.umlAbstraction.addClassAttribute (slot.measureType, "time", main.umlAbstraction.timeType, true);
-		}
 	}
 	
 	private void cIn_qOut (UMLAttributeSlot slot)
 	{
+		removeHistoryTracking(slot);
+		
 		if (slot.measurementAttribute != null)
 		{
 			// The "Characterized Type" remains...
-			
+				
 			// Remove the measurement attribute from the "Characterized Type"
-			main.umlAbstraction.removeClassAttribute(slot.measurementAttribute);
+			main.ui.writeLog("Removed attribute from " + slot.measurementAttribute.getClass_().getName() + ": " + slot.measurementAttribute.getName());
+			Log.removal();
 			
+			main.umlAbstraction.removeClassAttribute(slot.measurementAttribute);
+						
 			// Clear the UMLAttributeSlot
 			slot.measurementAttribute = null;
-			main.ui.writeLog("Removed UML.Property " + "(Measurement)");
-			Log.removal();
 		}
 	}
 	
 	private void cOut (UMLAttributeSlot slot)
 	{
+		removeHistoryTracking(slot);
+		
 		if (slot.measurementAttribute != null)
 		{			
+			main.ui.writeLog("Removed attribute from " + slot.measurementAttribute.getClass_().getName() + ": " + slot.measurementAttribute.getName());
+			Log.removal();
+			
 			// Clear the UMLAttributeSlot
 			slot.measurementAttribute = null;
-			main.ui.writeLog("Removed UML.Property " + "(Measurement)");
+		}
+	}
+	
+	
+	
+	/* ------------------------------------ History Tracking ------------------------------------ */
+	
+	private void addHistoryTracking(RefOntoUML.Quality q1, RefOntoUML.Class c1, MeasurementDecision decision, UMLAttributeSlot slot)
+	{
+		if (slot.measureType == null)
+		{
+			// create "Measure Type"
+			slot.measureType = main.umlAbstraction.createMeasureType(q1.getName() + " Measure");
+			
+			main.ui.writeLog("Created " + slot.measureType.getName());
+			Log.addition();
+			
+			// add the "Measurement Attribute" to the "Measure type"
+			slot.measurementAttribute = main.umlAbstraction.addMeasurementAttribute(q1, slot.measureType, decision);
+			
+			main.ui.writeLog("Created attribute for " + slot.measureType.getName() + ": " + slot.measurementAttribute.getName());
+			Log.addition();
+			
+			// create "Measure Association" (between the "Characterized Type" and the "Measure Type")
+			main.umlAbstraction.createMeasureAssociation((org.eclipse.uml2.uml.Class)Onto2InfoMap.getElement(c1), slot.measureType);
+		}
+		
+		if (main.dh.getStartTimeDecision(q1))
+		{
+			addTimeTracking(slot);
+		}
+	}
+	
+	private void removeHistoryTracking (UMLAttributeSlot slot)
+	{
+		if (slot.measureType != null)
+		{
+			// Remove the "Time attribute"
+			removeTimeTracking (slot);
+			
+			// Remove the "Measure Association"
+			main.umlAbstraction.removePackageableElement(slot.measureType.getAssociations().get(0));
+			
+			// Remove the "Measure Type"
+			main.umlAbstraction.removePackageableElement(slot.measureType);
+						
+			main.ui.writeLog("Removed " + slot.measureType.getName());
 			Log.removal();
+			
+			slot.measureType = null;
+		}
+	}
+	
+	
+	
+	/* ------------------------------------ Time Tracking ------------------------------------ */
+	
+	private void addTimeTracking(UMLAttributeSlot slot)
+	{
+		if (slot.startAttribute == null)
+		{
+			// create "Time Attribute"
+			slot.startAttribute = main.umlAbstraction.addClassAttribute (slot.measureType, "time", main.umlAbstraction.timeType, true);
+			
+			main.ui.writeLog("Created attribute for " + slot.measureType.getName() + ": " + slot.startAttribute.getName());
+			Log.addition();
+		}
+	}
+	
+	private void removeTimeTracking (UMLAttributeSlot slot)
+	{
+		if (slot.startAttribute != null)
+		{
+			// Remove the "Time attribute" from the "Measure Type"
+			slot.startAttribute.getClass_().getOwnedAttributes().remove(slot.startAttribute);
+			
+			main.ui.writeLog("Removed attribute from " + slot.measureType.getName() + ": " + slot.startAttribute.getName());
+			Log.removal();
+			
+			slot.startAttribute = null;
 		}
 	}
 }
